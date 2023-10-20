b5dfc934363b8a76bc86e171257cb3db23cbc12c
sristikundu1 <nidhukundu1234@gmail.com>
2023-10-20 16:56:36 +0600

    create a my cart page where people can see the product that he/she added

---
+++ src/Pages/MyCart/MyCart.jsx
@@ -0,0 +1,96 @@
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import Swal from "sweetalert2";
+
+
+const MyCart = () => {
+    const loadedwishProduct = useLoaderData();
+
+   
+    const [wishProducts, setWishProduct] = useState(loadedwishProduct);
+
+    const handleDelete = _id => {
+        console.log(_id);
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                fetch(`http://localhost:5000/wishproducts/${_id}`, {
+                    method: 'DELETE'
+
+                })
+                    .then(res => res.json())
+                    .then(data => {
+
+                        //delete one data use deletedCount > 0
+                        if (data.deletedCount > 0) {
+                            Swal.fire(
+                                'Deleted!',
+                                'Your product has been deleted.',
+                                'success'
+                            )
+
+                            //delete from the UI
+
+                            const remainingUsers = wishProducts.filter(cof => cof._id !== _id);// dont delete which id is not matched
+                            setWishProduct(remainingUsers);
+                        }
+                    })
+
+            }
+        })
+
+    }
+    return (
+        <div>
+            <h2>mauy cart{loadedwishProduct.length}</h2>
+
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            
+                            <th>Product</th>
+                            <th>Product Name,Size,Colore</th>
+                            <th></th>
+                            <th></th>
+                            <th>Price</th>
+                            <th>Brand Name</th>
+                            <th>Type</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            wishProducts.map(wishProduct => <tr key={wishProduct._id}>
+                                
+                                <td><img className="w-72 h-64" src={wishProduct.Photo} alt="" /></td>
+                                <td>{wishProduct.Name}</td>
+                                <td>{wishProduct.Size}</td>
+                                <td>{wishProduct.Colour}</td>
+                                <td>{wishProduct.Price}</td>
+                                <td>{wishProduct.BrandName}</td>
+                                <td>{wishProduct.Type}</td>
+                                <td>
+                                    <button onClick={() => handleDelete(wishProduct._id)} className="btn">X</button>
+                                </td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default MyCart;
